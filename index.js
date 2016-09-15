0eaeead4bf46ccf2c6b80cc9288d3c7715d38cf7
Russian Reboucas <russian.reboucas@gmail.com>
2016-09-14 22:22:47 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = function (promises) {
+
+  if (!Array.isArray(promises)) {
+    throw new Error('First argument need to be an array of Promises');
+  }
+
+  return new Promise((resolve, reject) => {
+
+    let count = 0;
+    let results = [];
+
+    const iterateeFunc = (previousPromise, currentPromise) => {
+      return previousPromise
+        .then(function (result) {
+          if (count++ !== 0) results = results.concat(result);
+          return result;
+        })
+        .then(currentPromise)
+        .catch((err) => {
+          return reject(err);
+        });
+    }
+
+    promises = promises.concat(() => Promise.resolve());
+
+    promises
+    .reduce(iterateeFunc, Promise.resolve(false))
+    .then(function (res) {
+      resolve(results);
+    })
+
+  });
+};
---
+++ test/index.js
@@ -0,0 +1,55 @@
+const expect = require("chai").expect;
+const promiseq = require('../index.js');
+
+describe('sequentially promises', () => {
+
+  it('should throw a error if not pass an array of promises', () => {
+    const func = () => {
+      promiseq({});
+    }
+    expect(func).to.throw('First argument need to be an array of Promises');
+  });
+
+  it('should return an Array as response', (done) => {
+
+    let promises = [1,2].map((item) => {
+
+      return function (previousResponse) {
+        return new Promise(resolve => {
+          setTimeout(function () {
+            resolve(item)
+          }, 1000)
+        });
+      }
+
+    });
+    const array = [1,2,3,4]
+    promiseq(promises)
+      .then(res => {
+        console.log(res);
+        expect(res).to.be.instanceof(Array);
+        done();
+      });
+
+  });
+
+  it('should return 1 as param of catch function', (done) => {
+
+    let promises = [1,2].map((item) => {
+      return function (previousResponse) {
+        return new Promise((r, reject) => {
+          setTimeout(() => {
+            reject(1)
+          }, 1000)
+        })
+      }
+    });
+
+    promiseq(promises)
+      .catch(err => {
+        expect(err).to.equal(1);
+        done();
+      })
+  });
+
+})
