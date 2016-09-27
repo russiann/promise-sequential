f0ddd17a6960c574bb124f653521d2de9d27b21b
Zeb McCorkle <zebmccorkle@gmail.com>
2016-09-27 16:28:20 -0500

    Make the module universal, fixes #3
    I didn't use a template as I don't have a Node.js environment installed locally in order to install Webpack. I took inspiration from the official [UMD templates](https://github.com/umdjs/umd) to craft this special version of UMD, made for this module.
    
    I attempted to follow your code style as much as possible.

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-module.exports = function (promises) {
+// IIFE in "optimized" format, see https://github.com/nolanlawson/optimize-js
+!(function (root) {
+  function promiseSequential (promises) {
 
-  if (!Array.isArray(promises)) {
-    throw new Error('First argument need to be an array of Promises');
-  }
-
-  return new Promise((resolve, reject) => {
-
-    let count = 0;
-    let results = [];
-
-    const iterateeFunc = (previousPromise, currentPromise) => {
-      return previousPromise
-        .then(function (result) {
-          if (count++ !== 0) results = results.concat(result);
-          return currentPromise(result, results, count);
-        })
-        .catch((err) => {
-          return reject(err);
-        });
+    if (!Array.isArray(promises)) {
+      throw new Error('First argument need to be an array of Promises');
     }
 
-    promises = promises.concat(() => Promise.resolve());
+    return new Promise((resolve, reject) => {
 
-    promises
-    .reduce(iterateeFunc, Promise.resolve(false))
-    .then(function (res) {
-      resolve(results);
-    })
+      let count = 0;
+      let results = [];
 
-  });
-};
+      const iterateeFunc = (previousPromise, currentPromise) => {
+        return previousPromise
+          .then(function(result) {
+            if (count++ !== 0) results = results.concat(result);
+            return currentPromise(result, results, count);
+          })
+          .catch((err) => {
+            return reject(err);
+          });
+      }
+
+      promises = promises.concat(() => Promise.resolve());
+
+      promises
+        .reduce(iterateeFunc, Promise.resolve(false))
+        .then(function(res) {
+          resolve(results);
+        })
+
+    });
+  }
+  
+  // Universal Module Definition
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define('promiseSequential', () => promiseSequential);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node.js
+    module.exports = promiseSequential;
+  } else {
+    // Browser/other
+    root.promiseSequential = promiseSequential;
+  }
+})(this);
